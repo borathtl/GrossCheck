65b1c40f997829ffcdbc6228915dab43698ad6f8
borathtl <boratahtali@gmail.com>
2020-05-19 02:22:21 +0300

    splash screen bug solved and its working (app manifest sorted)

--- app/src/main/java/com/example/grosscheck/MainActivity.java
+++ app/src/main/java/com/example/grosscheck/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
     private static int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
