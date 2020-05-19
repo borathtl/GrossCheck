8843285999415b59fc3e5be3e361b1c567d65810
borathtl <boratahtali@gmail.com>
2020-05-19 12:10:00 +0300

    unnecessary imports removed

--- app/src/main/java/com/example/grosscheck/MainActivity.java
+++ app/src/main/java/com/example/grosscheck/MainActivity.java
@@ -6,16 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.SignInAccount;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
@@ -88,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
+        // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
     }
