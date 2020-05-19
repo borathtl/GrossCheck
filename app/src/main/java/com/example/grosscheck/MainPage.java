f159dfee54b668f220840a9dbdd95ac78a26be1d
burakaslanbas <burakaslanbas@hotmail.com>
2020-05-19 11:47:42 +0300

    google signin

--- app/src/main/java/com/example/grosscheck/MainPage.java
+++ app/src/main/java/com/example/grosscheck/MainPage.java
@@ -3,8 +3,12 @@ package com.example.grosscheck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainPage extends AppCompatActivity {
 
@@ -15,6 +19,15 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+     GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+     if (acct != null) {
+         String personName = acct.getDisplayName();
+         String personGivenName = acct.getGivenName();
+         String personFamilyName = acct.getFamilyName();
+         String personEmail = acct.getEmail();
+         String personId = acct.getId();
+         Uri personPhoto = acct.getPhotoUrl();
+     }
 
         welcomeText= findViewById(R.id.welcome);
         Intent recieveIntent = getIntent();
