930d31a360035b7ef3c3e641fb92fa6e0b35c960
CerenOzgul <cereen_ozgull@hotmail.com>
2020-05-19 11:57:09 +0300

    push.cerenozgul

--- app/src/main/java/com/example/grosscheck/MainActivity.java
+++ app/src/main/java/com/example/grosscheck/MainActivity.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.Task;
 
+public class MainActivity extends AppCompatActivity {
+    int RC_SIGN_IN = 0;
     private Button loginButton;
     private Button withoutLogin;
     private boolean flag= false;
@@ -37,7 +41,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
    public void openMainPage()
     {
         Intent intent = new Intent(this, MainPage.class);
